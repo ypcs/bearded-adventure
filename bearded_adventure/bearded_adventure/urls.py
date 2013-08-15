32f0964cf6a591e347eb923f30e0bb6af340bfa8
Ville Korhonen <ville@xd.fi>
2013-08-15 19:51:26 +0300

    Updated API

--- bearded_adventure/bearded_adventure/urls.py
+++ bearded_adventure/bearded_adventure/urls.py
@@ -4,16 +4,19 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from tastypie.api import Api
+
 from webvm.api import VirtualMachineResource
 
-vm_resource = VirtualMachineResource()
+v1_api = Api(api_name='v1')
+v1_api.register(VirtualMachineResource())
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bearded_adventure.views.home', name='home'),
     # url(r'^bearded_adventure/', include('bearded_adventure.foo.urls')),
 
-    url(r'^api/', include(vm_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
