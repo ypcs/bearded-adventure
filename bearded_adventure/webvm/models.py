f0f633085215fbf9caa62bca79ebf6b7233e8412
Ville Korhonen <ville@xd.fi>
2013-08-15 17:03:50 +0300

    Created initial models for Slaves, Virtual Machines etc.

--- bearded_adventure/webvm/models.py
+++ bearded_adventure/webvm/models.py
@@ -1,3 +1,38 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django_extensions.db.fields import AutoSlugField, CreationDateTimeField, ModificationDateTimeField, UUIDField
+
+class HwConfiguration(models.Model):
+    uuid = UUIDField()
+    memory_size = models.PositiveIntegerField()
+    cpu_count = models.PositiveIntegerField()    
+
+class Slave(models.Model):
+    uuid = UUIDField()
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    owner = models.ForeignKey(User)
+
+class MachineImage(models.Model):
+    uuid = UUIDField()
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    
+    hardware = models.ForeignKey(HwConfiguration)
+    
+    url = models.URLField()
+    checksum = models.CharField(max_length=255)
+
+class VirtualMachine(models.Model):
+    uuid = UUIDField()
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    
+    machine_image = models.ForeignKey(MachineImage)
+    owner = models.ForeignKey(User)
+
+class Snapshot(models.Model):
+    uuid = UUIDField()
+    vm = models.ForeignKey(VirtualMachine)
